58c36dbf912f4a496570fde570e363c0b090be3c
Ian Jamieson <ian.jamieson@globalgaming.com>
2022-11-20 21:44:33 +0100

    start detail

--- pages/game/[slug].vue
+++ pages/game/[slug].vue
@@ -1,3 +1,47 @@
 <template>
   game slug: {{ $route.params.slug }}
+  <pre>{{ data.game[0] }}</pre>
+  <Image :image-id="data.game[0].header_image.id" />
 </template>
+
+<script lang="ts" setup>
+const route = useRoute();
+const slug = route?.params?.slug;
+console.log('route', slug);
+/**
+ * We get the initial data from our graphql backend
+ * Preferably we would use functions in the @nuxtjs/composition-api or /queries to get the data from the store
+ * */
+const query = gql`
+  query getGame($slug: String!) {
+    game(filter: { slug: { _eq: $slug } }) {
+      id
+      name
+      slug
+      header_image {
+       id
+      }
+      game_translations {
+        title
+        thumbnail {
+            id
+        }
+      }
+    }
+  }
+`
+const variables = { slug }
+const { data } = await useAsyncQuery(query, variables)
+
+/** test Pinia - seems to be buggy/crashing */
+/*const gamesStore = useGameStore();
+
+/* We can use the filtered data in our template. Client side only */
+let search = ref();
+function filteredGames() {
+  const gamesArray = games._value.game // for some reason we can't use games.game - thanks vue
+  const searchValue = search.value ? search.value.toLowerCase() : undefined;
+  return searchValue && gamesArray.filter(game => game.name.toLowerCase().includes(searchValue));
+}
+
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,14 @@
 <template>
     <input type="text" v-model="search" placeholder="Search games..." /> <pre>{{ search }}</pre>
   <Grid>
+      <!-- This is our client loaded content that comes from filtered games -->
       <div v-if="search&&filteredGames().length !== 0" v-for="game in filteredGames()" :key="game">
         <game-thumbnail :game="game" :width="214" :height="214" />
       </div>
       <div v-else-if="search&&filteredGames().length === 0">
         <p>No games found for {{ search }}}</p>
       </div>
+      <!-- This is our default view, this has to be here for our default server side rendered content/speed/SEO -->
       <div v-else v-for="game in games.game" :key="game.id">
         <game-thumbnail :game="game" :width="214" :height="214" />
       </div>
@@ -14,8 +16,11 @@
 </template>
 
 <script lang="ts" setup>
-
-/** We get the initial data from our graphql backend */
+/**
+ * We get the initial data from our graphql backend
+ * Preferably we would use functions in the @nuxtjs/composition-api or /queries to get the data from the store
+ * This would allow us to also use fragments and not have to worry about the data we get back
+ * */
 const query = gql`
   query getGames($limit: Int!) {
     game(limit: $limit) {
@@ -34,10 +39,10 @@ const query = gql`
 const variables = { limit: 10 }
 const { data: games } = await useAsyncQuery(query, variables)
 
-/** test Pinia - seems to be buggy */
+/** test Pinia - seems to be buggy/crashing */
 /*const gamesStore = useGameStore();
 
-/* We can use the filtered data in our template */
+/* We can use the filtered data in our template. Client side only */
 let search = ref();
 function filteredGames() {
   const gamesArray = games._value.game // for some reason we can't use games.game - thanks vue
